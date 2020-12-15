bf2dd3654ea95841d4d43a6a1f2b68dc88e13049
ftosin@botkeeper.com <felixsonyusuftosin@gmail.com>
2020-12-15 14:07:02 +0100

    more test

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const { assert, expect } = require('chai')
-const { Glass, StackBuilder }  = require('./index')
-
+const { Glass, StackBuilder } = require('./index')
 
 describe('Glass', () => {
   let glass
@@ -19,37 +18,31 @@ describe('Glass', () => {
       expect(overflow).to.equal(50)
     })
   })
-
 })
 
 describe('Stack Builder', () => {
- 
   let stack = new StackBuilder()
-
-  describe('addToStack', () => {
-   it('should add to stack', () => { 
+  beforeEach(() => {
+    glass = new Glass(0, 0)
     stack.addToStack([1])
-    stack.addToStack([2,4])
-    stack.addToStack([5,6, 7])
-    expect(stack.length).to.equal(3)
-    expect(stack.root.row).to.equal(0)
-
-   })
-
-
+    stack.addToStack([2, 4])
+    stack.addToStack([5, 6, 7])
+  })
+  describe('addToStack', () => {
+    it('should add to stack', () => {
+      expect(stack.length).to.equal(3)
+      expect(stack.root.row).to.equal(0)
+    })
   })
   describe('_convertArrayToGlass', () => {
-
+    it ('should return an array of glass ', () => { 
+      const glasses = stack._convertArrayToGlass([1,2,3])
+      expect(glasses[0] instanceof Glass).to.be.true
+      expect(glasses[1] instanceof Glass).to.be.true
+      expect(glasses[2] instanceof Glass).to.be.true
+    })
   })
-  describe('getGlass', () => {
-
-  })
-  describe('simulatePour', () => {
-
-  })
-  describe('pourLiquidAndGetCupVolume', () => {
-
-  })
-  
-
+  describe('getGlass', () => {})
+  describe('simulatePour', () => {})
+  describe('pourLiquidAndGetCupVolume', () => {})
 })
